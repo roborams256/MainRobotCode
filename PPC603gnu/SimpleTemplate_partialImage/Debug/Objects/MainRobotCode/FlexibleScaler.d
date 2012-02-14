45ac6e055ca7ddefbf7b80864e3f61a81ae7818c
unknown <home@home-PC.(none)>
2012-02-14 11:58:25 -0800

    hopefully removed accidentally commited object files

--- PPC603gnu/SimpleTemplate_partialImage/Debug/Objects/MainRobotCode/FlexibleScaler.d
+++
@@ -1,5 +0,0 @@
-SimpleTemplate_partialImage/Debug/Objects/MainRobotCode/FlexibleScaler.o:  \
- C:/WindRiver/workspace/MainRobotCode/FlexibleScaler.cpp \
-  C:/WindRiver/workspace/MainRobotCode/FlexibleScaler.h
-
-C:/WindRiver/workspace/MainRobotCode/FlexibleScaler.h:
